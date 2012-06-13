a94d91f575096f9d9fc0a2415814593e26fc970f
Oleg <markelog@gmail.com>
2012-06-13 11:14:21 +0400

    Add first tests

---
+++ test/common/common.js
@@ -0,0 +1,3 @@
+this.moduleTeardown = function() {
+    localStorage.clear();
+}
---
+++ test/storages/localstorage.js
@@ -0,0 +1,73 @@
+module("lc", {
+    teardown: moduleTeardown
+});
+
+test( "Simple set and get", function() {
+    var values = {
+        text: "test",
+        json: { test: "test" }
+    }
+
+    jar().set( "text", values.text ).done(function() {
+        this.get( "text" )
+            .done(function( data, type ) {
+                strictEqual( data, values.text, "set and get ok for text type" );
+                strictEqual( type, "text", 'type argument should be set to "text"' );
+            })
+            .fail(function() {
+                ok( false, "fail should not be called for successful operation" );
+            })
+    });
+
+    jar().set( "json", values.json ).done(function() {
+        this.get( "json" )
+            .done(function( data, type ) {
+                strictEqual( data.test, "test", "set and get ok for json type" );
+                strictEqual( type, "json", 'type argument should be set to "json"' );
+            })
+            .always(function() {
+                ok( true, "always should be called for successful operation" );
+            })
+    });
+
+    jar().get( "not-existed" )
+        .fail(function( data ) {
+            ok( true, "fail should be called" );
+        })
+        .always(function() {
+            ok( true, "always should be called for fail operation" );
+        })
+});
+
+test( "Remove method", function() {
+    expect( 1 );
+
+    jar().set( "test", "test" ).done(function() {
+
+        // Make sure data is writed
+        this.get( "test" ).done(function( data ) {
+            this.remove( "test" ).done(function( data ) {
+                this.get( "bla" )
+                    .fail(function() {
+                        ok( true, "data was successfully removed" );
+                    }).done( function() {
+                        ok( false, "data was not removed" );
+                    });
+            });
+        })
+    });
+});
+
+test( "Differentiate data in diffrent data-sets", function() {
+    expect( 1 );
+
+    jar( "test-1" ).set( "test", "test1" ).done(function() {
+        this.get( "test" ).done(function( test1 ) {
+            jar( "test-2" ).set( "test", "test2" ).done(function() {
+                this.get( "test" ).done(function( test2 ) {
+                    ok( test1 !== test2, "In diffrent data-sets diffrent data" );
+                });
+            });
+        });
+    });
+});
