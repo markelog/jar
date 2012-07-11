ebed2a23d093052e4e677e7bf4f9a1a70b90df02
Oleg <markelog@gmail.com>
2012-07-12 02:06:35 +0400

    Add test for jar.load for css data-type

--- test/load.js
+++ test/load.js
@@ -18,6 +18,8 @@ asyncTest( "jar.load - js", function() {
                 // wait a bit, so data can be writed
                 window.setTimeout(function() {
 
+                    ok( jar.has( "jar.load - js", path ), "Meta-data should be intializated" );
+
                     // through jar-store
                     jar.load( path, "js", "jar.load - js" ).done(function( data, base ) {
                         ok( data.length, "Data is returned" );
@@ -49,9 +51,41 @@ asyncTest( "jar.load - xml", function() {
                 // wait a bit, so data can be writed
                 window.setTimeout(function() {
 
+                    ok( jar.has( "jar.load - xml", path ), "Meta-data should be intializated" );
+
                     // through jar-store
                     jar.load( path, "xml", "jar.load - xml" ).done(function( data, base ) {
                         strictEqual( jar.type( data ), "xml", "Data is returned" );
+                        ok( base instanceof jar, "Second argument is instace of jar" );
+
+                        start();
+                    });
+                }, 100 );
+            });
+        });
+    });
+});
+
+asyncTest( "jar.load - css", function() {
+    var path = "//" + window.location.host + window.location.pathname + "data/data.css";
+
+    jar( "jar.load - css" ).done(function() {
+        this.clear().done(function() {
+
+            // through xhr-request
+            jar.load( path, "css", "jar.load - css" ).done(function( data, base ) {
+                var element = jQuery( '<div class="load-test"></div>' ).appendTo( "#qunit-fixture" );
+                strictEqual( element.css( "font-size" ), "99px", "css style should be applied" );
+
+                ok( base instanceof jar, "Second argument is instace of jar" );
+
+                // wait a bit, so data can be writed
+                window.setTimeout(function() {
+
+                    ok( jar.has( "jar.load - css", path ), "Meta-data should be intializated" );
+
+                    // through jar-store
+                    jar.load( path, "css", "jar.load - css" ).done(function( data, base ) {
                         ok( base instanceof jar, "Second argument is instace of jar" );
 
                         start();
