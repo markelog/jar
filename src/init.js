e6178708dbfaf21888921aaff03547626fcdc7f2
Oleg <markelog@gmail.com>
2012-07-23 18:59:38 +0400

    Make module init more smart Very sad day, now we use browser detection for two browsers, unfortunately there is no other way

--- src/init.js
+++ src/init.js
@@ -1,24 +1,30 @@
 !function() {
-    var preferable = {
-        js: [ "fs", "idb", "sql", "lc" ],
-        css: [ "fs", "idb", "sql", "lc" ],
-        xml: [ "fs", "idb", "sql", "lc" ],
-        html: [ "idb", "sql", "lc" ],
-        text: [ "sql", "idb", "lc" ],
-        json: [ "idb", "sql", "lc" ]
-    };
 
-    jar.preference = function( preferences ) {
-        var moz, value, preference,
+    // Yeah, stupid, ugly browser detection, but Firefox ask user permission to use IndexedDB
+    // we can't possibly detect that
+    var isMoz = !!~window.navigator.userAgent.indexOf( "Firefox" ),
+        vendor = window.navigator.vendor,
+        preferable = {
+            js: [ "fs", "idb", "sql", "lc" ],
+            css: [ "fs", "idb", "sql", "lc" ],
+            xml: [ "fs", "idb", "sql", "lc" ],
+            html: [ "idb", "sql", "lc" ],
+            text: [ "sql", "idb", "lc" ],
+            json: [ "idb", "sql", "lc" ]
+        };
+
+    // Yeah, stupid, ugly browser detection, but localStorage in Safari extremely (very, very) fast
+    // we can't possibly detect that (actually we can, but its crazy way)
+    if ( vendor && ~vendor.indexOf( "Apple" ) ) {
+        preferable.js = preferable.css = preferable.xml = preferable.html = preferable.text = preferable.json = [ "lc" ];
+    }
+
+    jar.preference = function( preferences, moz ) {
+        var value, preference,
             order = {},
             storages = jar.fn.storages.toString();
 
-        if ( preferences ) {
-            moz = true;
-
-        } else {
-            preferences = preferable;
-        }
+        preferences = preferences || preferable;
 
         for ( var type in preferences ) {
             preference = preferences[ type ];
@@ -31,7 +37,7 @@
 
                     // Firefox ask user permission to use indexedDb, because of this, we don't use it in FF
                     // if argument is default argument
-                    if ( value == "idb" && !moz && window.mozIndexedDB ) {
+                    if ( value == "idb" && !moz && isMoz ) {
                         continue;
                     }
 
--- test/init.js
+++ test/init.js
@@ -3,6 +3,8 @@ module( "init", {
 });
 
 test( "jar.preference", function() {
+    var isMoz = !!~window.navigator.userAgent.indexOf( "Firefox" );
+
     jar.preference({
         js: [ "lc" ],
         css: [ "lc" ]
@@ -15,24 +17,18 @@ test( "jar.preference", function() {
     strictEqual( jar.order.js, jar.order.javascript, '"javascript" prop should be alias to "js" prop' );
     ok( !jar.order.xml, "With new preference this prop should not be exist" );
 
-    if ( !window.mozIndexedDB ) {
-        window.mozIndexedDB = "test";
+    if ( isMoz ) {
+        jar.preference({
+            js: [ "idb", "lc" ]
+        }, true );
+
+        strictEqual( jar.order.js[ 0 ], "idb",
+            "When user specified storages he wants to use limitations for idb storage in mozilla should not be applied when second argument is provided" );
     }
-
-    jar.preference({
-        js: [ "idb", "lc" ]
-    });
-
-    strictEqual( jar.order.js[ 0 ], "idb",
-        "When user specified storages he wants to use limitations for idb storage in mozilla should not be applied" );
 
     jar.preference();
 
     ok( jar.order.xml, "Settings should get back to default value" );
 
-    // cleanup
-    if ( window.mozIndexedDB == "test" ) {
-        window.mozIndexedDB = undefined;
-    }
 });
 
