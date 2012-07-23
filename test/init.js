e9eb97dfeddfa0bc823c51e5fb12441d07db46ab
Oleg <markelog@gmail.com>
2012-07-23 19:20:22 +0400

    Add more tests

--- test/init.js
+++ test/init.js
@@ -3,7 +3,9 @@ module( "init", {
 });
 
 test( "jar.preference", function() {
-    var isMoz = !!~window.navigator.userAgent.indexOf( "Firefox" );
+    var isMoz = !!~window.navigator.userAgent.indexOf( "Firefox" ),
+        vendor = window.navigator.vendor,
+        xml = jar.order.xml[ 0 ];
 
     jar.preference({
         js: [ "lc" ],
@@ -11,8 +13,6 @@ test( "jar.preference", function() {
     });
 
     strictEqual( jar.order.js[ 0 ], "lc", 'This value should be "lc"' );
-    strictEqual( jar.order.js[ 0 ], "lc", 'This value should be "lc"' );
-    strictEqual( jar.order.css[ 0 ], "lc", 'This value should be "lc"' );
     strictEqual( jar.order.css[ 0 ], "lc", 'This value should be "lc"' );
     strictEqual( jar.order.js, jar.order.javascript, '"javascript" prop should be alias to "js" prop' );
     ok( !jar.order.xml, "With new preference this prop should not be exist" );
@@ -28,7 +28,17 @@ test( "jar.preference", function() {
 
     jar.preference();
 
-    ok( jar.order.xml, "Settings should get back to default value" );
+    // In safari and Mozilla all types of data should be set to localStorage
+    if ( vendor && ~vendor.indexOf( "Apple" ) || isMoz ) {
+        strictEqual( jar.order.js[ 0 ], "lc", 'This value should be "lc"' );
+        strictEqual( jar.order.html[ 0 ], "lc", 'This value should be "lc"' );
+        strictEqual( jar.order.xml[ 0 ], "lc", 'This value should be "lc"' );
+        strictEqual( jar.order.json[ 0 ], "lc", 'This value should be "lc"' );
+        strictEqual( jar.order.text[ 0 ], "lc", 'This value should be "lc"' );
+        strictEqual( jar.order.js, jar.order.javascript, '"javascript" prop should be alias to "js" prop' );
+    }
+
+    strictEqual( jar.order.xml[ 0 ], xml, "Settings should get back to default value" );
 
 });
 
